3fd41571d67477047565d0ada419b24cd98ddf06
Robert Takeo <robert.takeo@pricerunner.com>
2022-03-31 15:41:04 +0200

    Adding tab to thumbs on next layer

--- keyboards/planck/keymaps/takikoo/keymap.c
+++ keyboards/planck/keymaps/takikoo/keymap.c
@@ -51,7 +51,7 @@ enum planck_keycodes {
 #define SPCMOV LT(_MOV, KC_SPC)
 #define SPCNUM LT(_NUM, KC_SPC)
 #define TABEXT LT(_EXTRA, KC_TAB)
-#define ZCTRL  LCTL_T(KC_Z)
+#define TABNUM LT(_NUM, KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -65,7 +65,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Hyper| Meh  | Alt  | GUI  |Lower |SpcNum|SpcMov|Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
- *  TabNum - Tab when tapped, NUM layer when held
  *  C-Esc - CTRL when held, ESC when tapped
  *  SpaceMove - Space when tapped, MOVE layer when held
  */
@@ -84,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   -  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Hyper| Meh  | Alt  | GUI  |Lower |SpcNum|SpcMov|Raise | Left | Down |  Up  |Right |
+ * | Hyper| Meh  | Alt  | GUI  |Lower |TabNum|SpcMov|Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  *  TabNum - Tab when tapped, NUM layer when held
  *  C-Esc - CTRL when held, ESC when tapped
@@ -94,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TABEXT,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    SE_ARNG,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SE_ODIA, SE_ADIA,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    SE_COMM, SE_DOT,  SE_MINS, KC_ENT ,
-    KC_HYPR, KC_MEH,  KC_LALT, KC_LGUI, BS_LOW,  SPCNUM,  SPCMOV,  ENT_RS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_HYPR, KC_MEH,  KC_LALT, KC_LGUI, BS_LOW,  TABNUM,  SPCMOV,  ENT_RS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 #define GLOW   MO(_GAMELOW)
