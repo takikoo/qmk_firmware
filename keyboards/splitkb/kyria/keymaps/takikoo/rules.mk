215bb39327e7989b464b9d8ae7c463455787bad3
Robert Takeo <robert.takeo@pricerunner.com>
2022-04-05 15:15:21 +0200

    Reducing firmware size

--- keyboards/splitkb/kyria/keymaps/takikoo/rules.mk
+++ keyboards/splitkb/kyria/keymaps/takikoo/rules.mk
@@ -2,3 +2,12 @@ OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
