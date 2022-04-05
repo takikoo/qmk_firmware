8f4b21a48bcc9ebcb61a740d59a699bd58b0d71d
Robert Takeo <robert.takeo@pricerunner.com>
2022-04-05 10:58:10 +0200

    Changing space to tab on thumb row

--- keyboards/splitkb/kyria/keymaps/takikoo/keymap.c
+++ keyboards/splitkb/kyria/keymaps/takikoo/keymap.c
@@ -45,7 +45,7 @@ enum custom_keycodes {
 #define ENT_RS   LT(_RAISE, KC_ENT)
 #define NAV      MO(_NAV)
 #define SPCNAV   LT(_NAV, KC_SPC)
-#define SPCNUM   LT(_NUM, KC_SPC)
+#define TABNUM   LT(_NUM, KC_TAB)
 #define FKEYS    MO(_FUNCTION)
 #define ADJUST   MO(_ADJUST)
 #define GLow     MO(_GAMELOW)
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,   KC_I ,   KC_O  , KC_P   , KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,   KC_K ,   KC_L  , SE_ODIA, SE_ADIA,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,   SE_COMM, SE_DOT, SE_MINS, KC_ENT,
-                                ADJUST , KC_LALT,  BS_LOW, SPCNUM ,KC_LGUI,     KC_MEH , SPCNAV , ENT_RS, KC_RGUI, KC_APP
+                                ADJUST , KC_LALT,  BS_LOW, TABNUM ,KC_LGUI,     KC_MEH , SPCNAV , ENT_RS, KC_RGUI, KC_APP
     ),
 
 /*
