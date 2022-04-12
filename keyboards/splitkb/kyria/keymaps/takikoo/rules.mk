0219b02cf36f09ce59d41cb982969a36ad7085b7
Robert Takeo <robert.takeo@pricerunner.com>
2022-04-12 22:40:02 +0200

    Adding copy/paste and leader
    asdfasd

--- keyboards/splitkb/kyria/keymaps/takikoo/rules.mk
+++ keyboards/splitkb/kyria/keymaps/takikoo/rules.mk
@@ -2,6 +2,7 @@ OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+LEADER_ENABLE = yes        # Enable the Leader Key feature
 
 LTO_ENABLE = yes
 CONSOLE_ENABLE = no
