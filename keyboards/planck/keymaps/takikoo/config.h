7402a1c87c0ab970143f6adfe82f88a7d553c376
takikoo <rtakeo@gmail.com>
2022-08-29 10:02:31 +0200

    updates

--- keyboards/planck/keymaps/takikoo/config.h
+++ keyboards/planck/keymaps/takikoo/config.h
@@ -25,3 +25,14 @@
 #endif
 
 #define LEADER_TIMEOUT 300
+
+// default but used in macros
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
