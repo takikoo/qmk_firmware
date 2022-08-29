02c60519ec7d3e0b990a1af4ee46b4c9f8ebaeec
takikoo <rtakeo@gmail.com>
2022-08-29 10:31:39 +0200

    fixes

--- keyboards/splitkb/kyria/keymaps/takikoo/encoder_map.c
+++ keyboards/splitkb/kyria/keymaps/takikoo/encoder_map.c
@@ -1,9 +1,14 @@
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [_QWERTY] = { ENCODER_CCW_CW(UC_STAB, UC_TAB)    ,  ENCODER_CCW_CW(KC_PGUP, KC_PGDN)                    },
-    [_LOWER]  = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)  ,  ENCODER_CCW_CW(KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP)  },
-    [_RAISE]  = { ENCODER_CCW_CW(S(KC_F3), KC_F3)    ,  ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)                    },
-    [_NAV]    = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)  ,  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT)                   },
-    [_NUM]    = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)  ,  ENCODER_CCW_CW(C(KC_Z), C(KC_Y))                    },
+    [_QWERTY]     = { ENCODER_CCW_CW(UC_STAB, UC_TAB)    ,  ENCODER_CCW_CW(KC_PGUP, KC_PGDN)                    },
+    [_GAME]       = { ENCODER_CCW_CW(UC_STAB, UC_TAB)    ,  ENCODER_CCW_CW(KC_PGUP, KC_PGDN)                    },
+    [_COLEMAK_DH] = { ENCODER_CCW_CW(UC_STAB, UC_TAB)    ,  ENCODER_CCW_CW(KC_PGUP, KC_PGDN)                    },
+    [_LOWER]      = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)  ,  ENCODER_CCW_CW(KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP)  },
+    [_RAISE]      = { ENCODER_CCW_CW(S(KC_F3), KC_F3)    ,  ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)                   },
+    [_NAV]        = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)  ,  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT)                   },
+    [_FUNCTION]   = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX)   ,  ENCODER_CCW_CW(XXXXXXX, XXXXXXX)                    },
+    [_NUM]        = { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)  ,  ENCODER_CCW_CW(C(KC_Z), C(KC_Y))                    },
+    [_GAMELOW]    = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX)   ,  ENCODER_CCW_CW(XXXXXXX, XXXXXXX)                    },
+    [_ADJUST]     = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX)   ,  ENCODER_CCW_CW(XXXXXXX, XXXXXXX)                    },
 };
 #endif
